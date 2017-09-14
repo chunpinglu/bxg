9bd72d54a54ec36de74cb6f5f3df6758d03a90b4
xioa <xioa@com>
2017-09-14 22:14:10 +0800

    修改bug版

--- views/static/js/teacher/add.js
+++ views/static/js/teacher/add.js
@@ -9,85 +9,10 @@ define(['jquery','template','utils','bootstrap','datepicker','datepickerCN','val
                 title:"讲师添加",
                 btnText:"添 加"
             };
-            $(".body,.teacher").html(template("edit-tpl",obj));
-
-             //使用datepicker插件
-             $("input[name=tc_join_date]").datepicker({
-                autoclose:true,
-                format:'yyyy-mm-dd',
-                language:'zh-CN'
-            })
-
-            //使用validate进行表单的验证
-            $("form").validate({
-                sendForm:false,
-                onBlur:true,
-                valid:function(){
-                    // $(".add-btn").click(function(){
-                        var data=$("form").serialize();
-                        $.ajax({
-                            url:"/api/teacher/add",
-                            type:"post",
-                            data:data,
-                            success:function(data){
-                                if(data.code==200){
-                                    console.log(data);
-                                    location.href="/teacher/teacher_list";
-                                }
-                            }
-                        })
-                    // })
-
-                },
-                description:{
-                    name:{
-                        required:"姓名不能为空"
-                    },
-                    password:{
-                        required:"密码不能为空",
-                        pattern:"请输入6到15位的字母或数字"
-                    },
-                    date:{
-                        required:"请输入入职日期"
-                    }
-                },
-                eachValidField:function(){
-                    // console.log($(this));
-                    $(this).parent().parent().removeClass("has-error").addClass("has-success");
-
-                },
-                eachInvalidField:function(){
-                    $(this).parent().parent().removeClass("has-success").addClass("has-error");
-
-                }
-
-
-            })
-
-            // $(".add-btn").click(function(){
-
-            //     var data=$("form").serialize();
-            //     $.ajax({
-            //         url:"/api/teacher/add",
-            //         type:"post",
-            //         data:data,
-            //         success:function(data){
-            //             if(data.code==200){
-            //                 console.log(data);
-            //                 location.href="/teacher/teacher_list";
-            //             }
-            //         }
-            //     })
-
-            // })
+            var url="/api/teacher/add";
+           renderData(obj,url);
             
         }else{
-            //2.编辑讲师
-            // var obj={
-            //     title:"讲师编辑",
-            //     btnText:"保 存"
-            // }
-            // $(".body,.teacher").html(template("edit-tpl",obj));
             $.ajax({
                 url:"/api/teacher/edit",
                 type:"get",
@@ -99,30 +24,9 @@ define(['jquery','template','utils','bootstrap','datepicker','datepickerCN','val
                         console.log(data);
                         data.result.title="讲师编辑";
                         data.result.btnText="保 存";
-                        $(".body,.teacher").html(template("edit-tpl",data.result));
+                        var url="/api/teacher/update";
+                        renderData(data.result,url);
 
-                         //使用datepicker插件
-                        $("input[name=tc_join_date]").datepicker({
-                            autoclose:true,
-                            format:'yyyy-mm-dd',
-                            language:'zh-CN'
-                        })
-
-                        $(".add-btn").click(function(){                      
-                                            var data=$("form").serialize();
-                                            $.ajax({
-                                                url:"/api/teacher/update",
-                                                type:"post",
-                                                data:data,
-                                                success:function(data){
-                                                    if(data.code==200){
-                                                        console.log(data);
-                                                        location.href="/teacher/teacher_list";
-                                                    }
-                                                }
-                                            })
-                            
-                                        })
                     }
                 }
             })
@@ -130,12 +34,62 @@ define(['jquery','template','utils','bootstrap','datepicker','datepickerCN','val
         }
 
         
-            // //使用datepicker插件
-            // $("input[name=tc_join_date]").datepicker({
-            //     autoclose:true,
-            //     format:'yyyy-mm-dd',
-            //     language:'zh-CN'
-            // })
+            function renderData(data,url){
+                
+                $(".body,.teacher").html(template("edit-tpl",data));
+                
+                             //使用datepicker插件
+                             $("input[name=tc_join_date]").datepicker({
+                                autoclose:true,
+                                format:'yyyy-mm-dd',
+                                language:'zh-CN'
+                            })
+                
+                            //使用validate进行表单的验证
+                            $("form").validate({
+                                sendForm:false,
+                                onBlur:true,
+                                valid:function(){
+                                    var data=$("form").serialize();
+                                        $.ajax({
+                                            url:url,
+                                            type:"post",
+                                            data:data,
+                                            success:function(data){
+                                                if(data.code==200){
+                                                    console.log(data);
+                                                    location.href="/teacher/teacher_list";
+                                                }
+                                            }
+                                        })
+                
+                                },
+                                description:{
+                                    name:{
+                                        required:"姓名不能为空"
+                                    },
+                                    password:{
+                                        required:"密码不能为空",
+                                        pattern:"请输入6到15位的字母或数字"
+                                    },
+                                    date:{
+                                        required:"请输入入职日期"
+                                    }
+                                },
+                                eachValidField:function(){
+                                    // console.log($(this));
+                                    $(this).parent().parent().removeClass("has-error").addClass("has-success");
+                
+                                },
+                                eachInvalidField:function(){
+                                    $(this).parent().parent().removeClass("has-success").addClass("has-error");
+                
+                                }
+                
+                
+                            })
+
+            }
 
 
         
